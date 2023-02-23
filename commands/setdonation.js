f630247065022daf6ed68f21cd2145a74c083c27
Zapperdax <qa80986@gmail.com>
2023-02-23 20:21:47 +0500

    Added Set Donation To Work Only If User Has The Specific Role

--- commands/setdonation.js
+++ commands/setdonation.js
@@ -5,6 +5,19 @@ module.exports = {
     .setName("setdonation")
     .setDescription("Sets Donation Of The User For The Current Week"),
   async execute(interaction) {
-    await interaction.reply("I'm Under Construction");
+    const roleName = "donation-tracker";
+
+    const role = interaction.member.roles.cache.find(
+      (r) => r.name === roleName
+    );
+    if (!role || !interaction.member.roles.cache.has(role.id)) {
+      await interaction.reply({
+        content: `You Don't Have Permission To Use This Command`,
+        ephemeral: true,
+      });
+      return;
+    }
+
+    await interaction.reply("Command Executed");
   },
 };
