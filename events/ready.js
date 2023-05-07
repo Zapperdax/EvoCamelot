24e6260f5d2a6e8786ba5ec69cc3c06f5912c2e5
Zapperdax <qa80986@gmail.com>
2023-05-07 19:28:42 +0500

    Auto Dm Working

--- events/ready.js
+++ events/ready.js
@@ -7,12 +7,12 @@ module.exports = {
   once: true,
   execute(client) {
     console.log(`Logged In As ${client.user.tag}`);
-
     const channel = client.channels.cache.get("813262057331884032");
     if (!channel) {
       console.log("No Channel Found");
       return;
     }
+
     const job0 = new cron.CronJob(
       "0 0 4 * * SUN",
       async () => {
@@ -25,22 +25,53 @@ module.exports = {
           channel.send(
             "List Of Users Who Didn't Donate This Week -> " + pingTheseUsers
           );
-          User.updateMany({}, { amount: 0, donated: false }, (err) => {
-            if (err) {
-              console.error(err);
-              return;
-            }
+        }
+        User.updateMany({}, { amount: 0, donated: false }, (err) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
 
-            channel.send(
-              `Weekly Donations Have Been Resetted, You Guys Can Start Donating For This Week Now ${"<@&740824003932848199>"}`
+          channel.send(
+            `Weekly Donations Have Been Resetted, You Guys Can Start Donating For This Week Now ${"<@&740824003932848199>"}`
+          );
+        });
+      },
+      null,
+      true,
+      "Europe/London"
+    );
+    const job1 = new cron.CronJob(
+      "0 0 4 * * FRI",
+      async () => {
+        const nonDonatedUsers = await User.find({ donated: false });
+        if (nonDonatedUsers.length > 0) {
+          const promises = nonDonatedUsers.map(async (user) => {
+            const userId = user.id;
+            const userToDm = await client.users.fetch(userId);
+            const dmChannel = await userToDm.createDM();
+            return dmChannel.send(
+              "Hi, I'm Here To Remind You Of Your Pending Weekly Donation In Evo's Lair ^^"
             );
           });
+          Promise.all(promises)
+            .then(() => {
+              console.log("Reminders sent to non-donators.");
+              channel.send(
+                "Reminder Has Been Sent To Everyone Who Haven't Donated"
+              );
+            })
+            .catch((err) => {
+              console.error(err);
+            });
         }
       },
       null,
       true,
       "Europe/London"
     );
+
     job0.start();
+    job1.start();
   },
 };
