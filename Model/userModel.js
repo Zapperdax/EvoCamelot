ee2e491cb80a438921dffed03843ee4536a606ec
Zapperdax <qa80986@gmail.com>
2023-09-08 10:32:56 +0500

    User Model Update

--- Model/userModel.js
+++ Model/userModel.js
@@ -16,6 +16,10 @@ const userSchema = new mongoose.Schema({
     type: Boolean,
     default: false,
   },
+  extraWeeks: {
+    type: Number,
+    default: 0,
+  }
 });
 
 const User = mongoose.model("User", userSchema);
